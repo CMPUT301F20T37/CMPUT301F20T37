497aec0867cdb0fe7d85126a708b988da759cf88
jchristiangayle <jchristiangayle@gmail.com>
2020-10-04 17:48:03 -0600

    added constructors to Circle class

--- CMPUT301F20T37/app/src/main/java/com/example/cmput301f20t37/Circle.java
+++ CMPUT301F20T37/app/src/main/java/com/example/cmput301f20t37/Circle.java
@@ -1,3 +1,14 @@
+package com.example.cmput301f20t37;
+
 public class Circle extends Shape {
-    
+
+    private float radius;
+
+    public Circle() {
+        this.radius = 0.0f;
+    }
+
+    public Circle(float radius) {
+        this.radius = radius;
+    }
 }
