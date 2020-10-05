04258d57501035a9d5a65c207a117a86901d8f25
TheSTruong <svtruong@ualberta.ca>
2020-10-04 18:03:58 -0600

    Added square class

---
+++ CMPUT301F20T37/app/src/main/java/com/example/cmput301f20t37/Square.java
@@ -0,0 +1,11 @@
+package com.example.cmput301f20t37;
+
+public class Square extends Shape {
+
+    private int length;
+    private int area;
+
+    public Square(int length) {
+        this.area = (length) * 4;
+    }
+}
